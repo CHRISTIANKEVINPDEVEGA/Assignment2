bebced99349596486fb773641b3c0d5853440bc3
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-10-24 22:36:30 +0800

    slight changes in 2nd program -pesos change to ₱

--- program_2.py
+++ program_2.py
@@ -1,10 +1,10 @@
 apple = int(input("How many apples do you want to buy? "))
 NumberOfApple_X_ApplePrice= apple*20
-print(f"₱20  X {apple} = ₱{NumberOfApple_X_ApplePrice}")
+print(f"₱20 X {apple} = ₱{NumberOfApple_X_ApplePrice}")
 
 orange = int(input("How many oranges do you want to buy? "))
 NumberOfOrange_X_OrangePrice= orange*25
-print(f"₱25  X {orange} = ₱{NumberOfOrange_X_OrangePrice}")
+print(f"₱25 X {orange} = ₱{NumberOfOrange_X_OrangePrice}")
 
 total_amount = NumberOfApple_X_ApplePrice + NumberOfOrange_X_OrangePrice
 print(f"The total amount is ₱{total_amount}.")
