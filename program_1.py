60a0d4cdab4ddcfd8d2d39811a9312d96977c675
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-10-24 19:19:40 +0800

    1st program -ask user questions

---
+++ program_1.py
@@ -0,0 +1,4 @@
+Name = input("what is your name? ") 
+Age = input("what is your age? ") 
+Address = input("what is your address? ") 
+print(f"Hi, my name is {Name}. I am {Age} years old and I live in {Address}.")
